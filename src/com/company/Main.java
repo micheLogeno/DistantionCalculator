a6684faad4af8025fdf44c979ee26c50b37ba326
micheLogeno <99113730+micheLogeno@users.noreply.github.com>
2022-02-12 11:48:18 +0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
+/*
+class Point{
+    private double x;
+    private double y;
+
+    public Point(double x1, double y1) {
+        this.x = x1;
+        this.y = y1;
+    }
+
+    public double distance(Point p2){
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}*/
+
